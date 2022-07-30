90a2464f7269608c31caac52b4df560669d2e2f8
easylager <oceandrinkparadise@gmail.com>
2022-07-30 16:55:15 +0300

    3

--- request_test.py
+++ request_test.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import urllib3
 
-
+'3'
 htttp_manager = urllib3.PoolManager()
 response_req = requests.get('https://www.kufar.by/l/r~minsk/noutbuki?clp=v.or%3A3%2C5%2C1&ot=0&prc=r%3A0%2C30000&query=%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA&sort=lst.d')
 response = htttp_manager.request('GET', 'https://www.kufar.by/l/r~minsk/noutbuki?clp=v.or%3A3%2C5%2C1&ot=0&prc=r%3A0%2C30000&query=%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA&sort=lst.d')
