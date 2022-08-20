38d457e33d55846490a2bd7208d4129e768a956a
easylager <oceandrinkparadise@gmail.com>
2022-08-20 21:56:54 +0300

    changed requests to urllib3

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import random
 import asyncio
 import requests
+import urllib3
 import re
+
 
 from enum import Enum
 from bs4 import BeautifulSoup
@@ -26,15 +28,19 @@ dp = Dispatcher(bot=bot)
 
 async def news_every_minute():
     while True:
-
-        response_req = requests.get(
+        http_manager = urllib3.PoolManager()
+        response = http_manager.request(
+            'GET',
+            'https://www.kufar.by/l/r~minsk/noutbuki?clp=v.or%3A3%2C5%2C1&ot=0&prc=r%3A0%2C30000&query=%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA&sort=lst.d'
+        )
+        """ response_req = requests.get(
             Goods.LAPTOPS,
             headers=UserAgentsHandler.header(),
             proxies=ProxiesHandler.proxy()
-        )
-        print(response_req)
-        bs_object = BeautifulSoup(response_req.text, 'lxml')
-        print(bs_object)
+        )"""
+
+        bs_object = BeautifulSoup(response.data, 'lxml')
+
         list_of_items = bs_object.find('div', {"data-name": "listings"})
 
         items = list_of_items.find_all('section')
